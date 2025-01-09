e9eae75eb5c82da9a69a55ac151b8f51f6e0733a
RJ Catalano <richard@thrackle.io>
2025-01-09 08:46:11 -0600

    add example user contract with min transfer file
    Signed-off-by: RJ Catalano <richard@thrackle.io>

---
+++ test/utils/ExampleUserContractWithMinTransferRevert.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+/**
+ * @title Example contract for testing the Rules Engine
+ * @dev This contract provides the ability to test a hard coded min transfer rule
+ * @author @ShaneDuncan602 
+ */
+contract ExampleUserContractWithMinTransferRevert {
+    event RulesEngineEvent(string _message);
+    uint256 predefinedMinTransfer;
+
+    constructor () {
+        predefinedMinTransfer = 4;
+    }
+
+    function transfer(address _to, uint256 _amount) public returns (bool _return) {
+        _to;
+        if (_amount < predefinedMinTransfer) {
+            revert();
+        } else {
+            return true;
+        }
+    }
+
+}
