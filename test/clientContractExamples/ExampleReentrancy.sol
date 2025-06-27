bec56187c0dad08cb2654c7aa6bd3197b130fc59
RJ Catalano <richard@thrackle.io>
2025-06-27 13:57:07 -0500

    cleaned up reentrancy but there appears to have been a bug discovered in foundry
    Signed-off-by: RJ Catalano <richard@thrackle.io>

--- test/clientContractExamples/ExampleReentrancy.sol
+++ test/clientContractExamples/ExampleReentrancy.sol
@@ -1,12 +1,18 @@
 pragma solidity ^0.8.27;
 
+import {RulesEngineComponentFacet} from "../../src/engine/facets/RulesEngineComponentFacet.sol";
+import {Trackers} from "../../src/engine/RulesEngineStorageStructure.sol";
 
-error ReentrancyFailed();
+error ReentrancyFailed(uint);
 
 contract ExampleReentrancy {
     bytes data;
     address to;
     bool delegatecall;
+    uint counter;
+    bytes[] public dataHistory;
+    bytes[] public dataHistory2;
+    address rulesEngine;
 
     fallback() external payable {
         if (data.length == 0) {
@@ -24,6 +30,10 @@ contract ExampleReentrancy {
         data = _data;
     }
 
+    function setRulesEngine(address _rulesEngine) public {
+        rulesEngine = _rulesEngine;
+    }
+
     function setDelegatecall(bool _delegatecall) public {
         delegatecall = _delegatecall;
     }
@@ -32,15 +42,31 @@ contract ExampleReentrancy {
         to = _to;
     }
 
+    function getDataHistory() public view returns (bytes[] memory) {
+        return dataHistory;
+    }
+
+    function getDataHistory2() public view returns (bytes[] memory) {
+        return dataHistory2;
+    }
+
     function reenter() internal {
+        counter++;
+        if (counter == 4) {
+            return;
+        }
         bool success;
         if (delegatecall) {
             (success, ) = address(to).delegatecall(data);
         } else {
             (success, ) = address(to).call(data);
         }
+        Trackers memory tracker = RulesEngineComponentFacet(rulesEngine).getTracker(1, 1);
+        dataHistory.push(tracker.trackerValue);
+        Trackers memory tracker2 = RulesEngineComponentFacet(rulesEngine).getTracker(2, 1);
+        dataHistory2.push(tracker2.trackerValue);
         if (!success) {
-            revert ReentrancyFailed();
+            revert ReentrancyFailed(counter);
         }
     }
 }
