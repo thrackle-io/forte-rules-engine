38abf09c12f1d24128671e56d97d771965c15df8
RJ Catalano <richard@thrackle.io>
2025-07-23 12:00:18 -0500

    validation and deleting unneccessary things to make size
    Signed-off-by: RJ Catalano <richard@thrackle.io>

--- src/engine/facets/RulesEngineForeignCallFacet.sol
+++ src/engine/facets/RulesEngineForeignCallFacet.sol
@@ -131,6 +131,13 @@ contract RulesEngineForeignCallFacet is FacetCommonImports {
         assert(_foreignCall.parameterTypes.length == _foreignCall.encodedIndices.length);
         require(_foreignCall.foreignCallIndex < MAX_LOOP, MAX_FC);
         require(_foreignCall.parameterTypes.length < MAX_LOOP, MAX_FC_PT);
+        uint mappedTrackerKeyIndexCounter = 0;
+        for (uint256 i = 0; i < _foreignCall.parameterTypes.length; i++) {
+            if (_foreignCall.encodedIndices[i].eType == EncodedIndexType.MAPPED_TRACKER_KEY) {
+                mappedTrackerKeyIndexCounter++;
+            }
+        }
+        require(mappedTrackerKeyIndexCounter == _foreignCall.mappedTrackerKeyIndices.length, "Mapped tracker key indices length mismatch.");
         lib._getForeignCallStorage().foreignCalls[_policyId][_foreignCallIndex] = _foreignCall;
         lib._getForeignCallStorage().foreignCalls[_policyId][_foreignCallIndex].set = true;
         lib._getForeignCallStorage().foreignCalls[_policyId][_foreignCallIndex].foreignCallIndex = _foreignCallIndex;
